ffdb8b541b141947403ba2c59a660187097d998f
Salvatore D'Agostino <salvatore.dagostino@sociablelabs.com>
2014-11-12 23:56:47 -0500

    added env and logging stuff
    reading env from environment var
    using logger

--- main.go
+++ main.go
@@ -2,14 +2,38 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/op/go-logging"
+	"log"
 	"os"
+	"runtime"
 )
 
 var (
-	port = os.Getenv("PORT")
+	APP_PORT   = os.Getenv("PORT")
+	APP_ENV    = os.Getenv("ENV")
+	LOG_FORMAT = os.Getenv("LOG_FORMAT")
+	LOGGER     *logging.Logger
 )
 
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	log.Printf("APP_ENV - %s = %s", "ENV", APP_ENV)
+	log.Printf("APP_PORT  - %s = %s", "PORT", APP_PORT)
+
+	if APP_ENV == "" || APP_PORT == "" {
+		log.Printf("Please define application environment and port with global variables ENV: %s (local,testing,staging,production) and PORT: %s respectively \n", APP_ENV, APP_PORT)
+		APP_ENV = "production" // Default to production if not set...
+	}
+}
+
 func main() {
+
+	LOGGER = logging.MustGetLogger("jollyHelper")
+	logBackend := logging.NewLogBackend(os.Stdout, "", 0)
+	logging.SetFormatter(logging.MustStringFormatter(LOG_FORMAT))
+	logging.SetBackend(logBackend)
+
 	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
 		c.String(200, "hello world")
@@ -23,5 +47,5 @@ func main() {
 	router.PUT("/error", func(c *gin.Context) {
 		c.String(500, "and error hapenned :(")
 	})
-	router.Run(":" + port)
+	router.Run(":" + APP_PORT)
 }
