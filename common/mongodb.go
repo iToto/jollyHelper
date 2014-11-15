79759ccac6f2ceb5b0b9d47c5534d4319d837f71
Salvatore D'Agostino <salvatore.dagostino@sociablelabs.com>
2014-11-15 16:56:09 -0500

    initial import of MongoDB handler

---
+++ common/mongodb.go
@@ -0,0 +1,25 @@
+package common
+
+import (
+	"github.com/gin-gonic/gin"
+	"gopkg.in/mgo.v2"
+	//"gopkg.in/mgo.v2/bson"
+)
+
+// MongoDBHandler returns a map of the gin context and the MongoDB instance
+func MongoDbHandler(url string, database string) gin.HandlerFunc {
+	session, err := mgo.Dial(url)
+	if err != nil {
+		panic(err)
+	}
+	return func(c *gin.Context) {
+		c.Set("mongoSession", session)
+
+		s2 := session.Clone()
+		c.Set("mongoStore", s2.DB(database))
+
+		defer s2.Close()
+
+		c.Next()
+	}
+}
