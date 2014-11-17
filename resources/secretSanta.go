0ae9735478c4ca25936bc18d439ae94cefcdba70
Salvatore D'Agostino <salvatore.dagostino@sociablelabs.com>
2014-11-17 13:42:22 -0500

    created secret santa array
    ready to run secret santa heuristic
    GitHub #4

--- resources/secretSanta.go
+++ resources/secretSanta.go
@@ -3,25 +3,20 @@ package resources
 import (
 	// "errors"
 	"github.com/gin-gonic/gin"
-	// "github.com/gin-gonic/gin/binding"
 	// "github.com/iToto/jollyHelper/common"
 	"github.com/iToto/jollyHelper/common/messagecode"
 	"github.com/iToto/jollyHelper/models"
-	"log"
-	// "github.com/op/go-logging"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
+	"log"
 	// "strconv"
-	// "time"
+	"time"
 )
 
 type SecretSantaResource struct {
 	Name       string
 	Owner      string
 	assignedOn int64
-}
-
-type Names struct {
 }
 
 func (ss *SecretSantaResource) AssignNames(c *gin.Context) {
@@ -31,20 +26,32 @@ func (ss *SecretSantaResource) AssignNames(c *gin.Context) {
 	log.Printf("Ho Ho Ho, it's name choosing time!")
 
 	// Get all names from db sorted by Age, Decreasing
-	person := &models.Person{}
+	personModel := &models.Person{}
 	persons := []models.Person{}
-	personCollection := mongoStore.C(person.Collection())
+	personCollection := mongoStore.C(personModel.Collection())
 
-	err = personCollection.EnsureIndex(person.Index())
+	err = personCollection.EnsureIndex(personModel.Index())
 
 	if err != nil {
 		sendError(&err, messagecode.E_SERVER_ERROR, c)
 		return
 	}
 
-	err = personCollection.Find(bson.M{}).Sort("-age").Limit(person.Limit()).All(&persons)
+	err = personCollection.Find(bson.M{}).Sort("-age").Limit(personModel.Limit()).All(&persons)
 	// log.Printf("List of people: %s", persons)
 
-	// TODO: Create names array list with
+	// Create names array list with
+	exchangeList := make([]*SecretSantaResource, 0)
+	for index, person := range persons {
+		// Create new secret santa name for each person
+		secretSantaName := &SecretSantaResource{}
+		secretSantaName.Name = person.Name
+		secretSantaName.Owner = ""
+		secretSantaName.assignedOn = time.Now().Unix()
+		exchangeList = append(exchangeList, secretSantaName)
+	}
+
+	log.Printf("Secret Santa List: %s", exchangeList)
+
 	// TODO: Assign a name to each person
 }
