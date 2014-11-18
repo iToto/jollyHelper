c1ecc72684d83ddd4881b64026e6a48d9a011a31
Salvatore D'Agostino <salvatore.dagostino@sociablelabs.com>
2014-11-17 22:07:29 -0500

    first version of the secret santa heuristic impl
    GitHub #4

--- resources/secretSanta.go
+++ resources/secretSanta.go
@@ -10,6 +10,7 @@ import (
 	"gopkg.in/mgo.v2/bson"
 	"log"
 	// "strconv"
+	"math/rand"
 	"time"
 )
 
@@ -42,16 +43,39 @@ func (ss *SecretSantaResource) AssignNames(c *gin.Context) {
 
 	// Create names array list with
 	exchangeList := make([]*SecretSantaResource, 0)
-	for index, person := range persons {
+	for _, person := range persons {
 		// Create new secret santa name for each person
 		secretSantaName := &SecretSantaResource{}
 		secretSantaName.Name = person.Name
 		secretSantaName.Owner = ""
-		secretSantaName.assignedOn = time.Now().Unix()
+		secretSantaName.assignedOn = 0
 		exchangeList = append(exchangeList, secretSantaName)
 	}
 
 	log.Printf("Secret Santa List: %s", exchangeList)
 
-	// TODO: Assign a name to each person
+	// Assign a name to each person
+	err = runSecretSanta(persons, exchangeList)
+	log.Printf("Secret Santa List Done: %s", exchangeList)
+}
+
+func runSecretSanta(persons []models.Person, secretSantaList []*SecretSantaResource) error {
+	// For every person, assign them a secret santa
+	rand.Seed(time.Now().UTC().UnixNano())
+	for index, person := range persons {
+		// Loop until unused name is chosen that is not current person
+		var i int
+		for i = randInt(0, len(persons)); (i == index) || (secretSantaList[i].assignedOn != 0); i = randInt(0, len(persons)) {
+			log.Printf("Re-choosing i: %i - assignedOn: %i", i, secretSantaList[i].assignedOn)
+		}
+		// Selected secret santa, update secretSanta list with secret santa
+		log.Printf("Name selected for %s", person.Name)
+		secretSantaList[i].assignedOn = time.Now().Unix()
+		secretSantaList[i].Owner = person.Name
+	}
+	return nil
+}
+
+func randInt(min int, max int) int {
+	return min + rand.Intn(max-min)
 }
