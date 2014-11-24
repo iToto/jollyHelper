c952f020477b1b8b7e1c513e4e8c81d0fdb38e29
Salvatore D'Agostino <salvatore.dagostino@sociablelabs.com>
2014-11-23 21:30:54 -0500

    now sending email to secret santa!
    using Mandril API
    
    Closes GitHub Issue #5

--- resources/notification.go
+++ resources/notification.go
@@ -6,6 +6,7 @@ import (
 	// "github.com/iToto/jollyHelper/common"
 	"github.com/iToto/jollyHelper/common/messagecode"
 	"github.com/iToto/jollyHelper/models"
+	"github.com/mostafah/mandrill"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 	"log"
@@ -20,6 +21,7 @@ type NotificationResource struct {
 
 func (n *NotificationResource) Send(c *gin.Context) {
 	// Get Secret Santa list
+
 	id := c.Params.ByName("id")
 	var err error
 	mongoStore := c.MustGet("mongoStore").(*mgo.Database)
@@ -39,13 +41,37 @@ func (n *NotificationResource) Send(c *gin.Context) {
 	}
 
 	err = secretSantaCollection.Find(bson.M{"uid": id}).One(&secretSantaList)
-	// log.Printf("List of people: %s", secretSantaList)
+	log.Printf("List of people: %s", secretSantaList)
 
 	// Create names array list with
 	for _, secretSanta := range secretSantaList.List {
 		// TODO: Send out secret santa for each person
 
 		log.Printf("Sending email to: %s with name: %s", secretSanta.Owner.Email, secretSanta.Name)
+		err = sendEmail(secretSanta.Owner.Email, secretSanta.Owner.Name, secretSanta.Name)
+
+		if err != nil {
+			sendError(&err, messagecode.E_SERVER_ERROR, c)
+			return
+		}
 	}
 
+}
+
+func sendEmail(recipientEmail string, recipientName string, name string) error {
+	log.Printf("Sending email to: %s with name: %s", recipientEmail, name)
+	msg := mandrill.NewMessageTo(recipientEmail, recipientName)
+	msg.Text = "Ho Ho Ho, Merry Christmas " + recipientName + "! This year, your secret Santa is: " + name // optional
+	msg.Subject = "2014 Secret Santa"
+	msg.FromEmail = "noreply@jollyhelper.com"
+	msg.FromName = "JollyHelper"
+	res, err := msg.Send(false)
+
+	log.Printf("Send email, response from Mandril: %s", res)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
