30e7d43afac8e84635b3f714f06548b6dff41e27
Salvatore D'Agostino <toto19@gmail.com>
2016-11-26 23:34:57 -0800

    add more properties to DialInfo
    use log.Fatalf instead of log + panic

--- common/mongodb.go
+++ common/mongodb.go
@@ -10,20 +10,21 @@ import (
 
 // MongoDBHandler returns a map of the gin context and the MongoDB instance
 func MongoDbHandler(hosts, database, username, password string) gin.HandlerFunc {
-	connInfo := mgo.DialInfo{
+	connInfo := &mgo.DialInfo{
 		Addrs:    []string{hosts},
+		Direct:   true,
 		Timeout:  60 * time.Second,
 		Database: database,
+		Source:   database,
 		Username: username,
 		Password: password,
 	}
 
 	log.Printf("Host:%s, DB:%s, Username:%s, PW:%s", hosts, database, username, password)
 	log.Printf("Connecting with info: %v", connInfo)
-	session, err := mgo.DialWithInfo(&connInfo)
+	session, err := mgo.DialWithInfo(connInfo)
 	if err != nil {
-		log.Printf("Could not connect to DB: %v", err)
-		panic(err)
+		log.Fatalf("CreateSession: %s\n", err)
 	}
 	log.Print("Successfully connected to Mongo")
 	return func(c *gin.Context) {
